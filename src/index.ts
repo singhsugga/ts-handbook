eeedae6f328443710b11669737c03655c35b17a8
wassef911 <1.wassef911@gmail.com>
2021-04-24 19:22:24 +0100

    testing js docs with TS

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,32 @@
-type Combinable = number | string;
-type ConversionType = "as-number"| "as-string";
 
-const add = (var1: Combinable ,var2:Combinable,ConvTarget : ConversionType)=> {
-    let result:any;
-    if(typeof var1 === "number" && typeof var2 === "number"  || ConvTarget === "as-number") {
+
+type Combinable = number | string;
+type ConversionType = "as-number" | "as-string";
+/**
+ * 
+ * @param {number} var1 first value to combine 
+ * @param {number} var2 first value to combine 
+ * @param {number} ConvTarget a type to convert result to 
+ * @returns {number | string}
+ */
+const add = (var1: Combinable, var2: Combinable, ConvTarget: ConversionType) => {
+    let result: any;
+    if (typeof var1 === "number" && typeof var2 === "number" || ConvTarget === "as-number") {
         result = +var1 + +var2;
-        if(result === "NaN") throw new Error("variables you passed are not valid numbers")
-    }else{
+        if (result === "NaN") throw new Error("variables you passed are not valid numbers")
+    } else {
         result = var1.toString() + var2.toString();
     }
     return result
 }
-(async()=>{
-    console.log(add(1,2,"as-number"));
+(async () => {
+    console.log(add(1, 2, "as-number"));
 })();
+
+/**
+ * @type {{task:string , done:boolean}}
+ */
+const todo = {
+    task: "do stuff",
+    done: true
+}
