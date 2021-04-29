aea96d571f147a71d79e5cc90bc16182176492fd
wassef911 <1.wassef911@gmail.com>
2021-04-29 23:18:18 +0100

    more types

---
+++ src/scratchTS/index.ts
@@ -0,0 +1,14 @@
+// @ts-expect-error
+const num: number = "ABC";
+
+const somethingReskhy = () => console.log("Booooo !");
+const assertIsError = (err: any): asserts err is Error => {
+    if (!(err instanceof Error)) throw new Error("Not an error : " + err);
+}
+
+try {
+    somethingReskhy();
+} catch (err: unknown) {
+    assertIsError(err);
+    console.log(err);
+}
--- src/scratchTS/types.ts
+++ src/scratchTS/types.ts
@@ -11,5 +11,18 @@ const y = x.user.adress?.city;
 // logs undefined if adress doesn't exist or city
 console.log(y);
 
-/* ------------------------------------------------- */
+/* -----------------------recursive type alias-------------------------- */
+type JsonValue = string | number | boolean | null | JsonValue[] | {
+    [k: string]: JsonValue;
+};
+
+const val: JsonValue = {
+    name: "mike",
+    adress: {
+        street: "Spear St"
+    }
+}
+/* -------------------------literal type------------------------ */
+
+type Corner = `${"top" | "bottom"}${"left" | "right"}`
 
