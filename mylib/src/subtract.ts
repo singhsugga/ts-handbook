2eb2f5cba6a37a7a7f24a49abded519893ab56b5
wassef911 <1.wassef911@gmail.com>
2021-05-01 00:28:24 +0100

    beta/private/public lib

---
+++ mylib/src/subtract.ts
@@ -0,0 +1,9 @@
+/**
+ * Calculate the difference between two numbers
+ *
+ * @param a - first number
+ * @param b - second number
+ *
+ * @beta
+ */
+export const subtract = (a: number, b: number): number => a - b;
